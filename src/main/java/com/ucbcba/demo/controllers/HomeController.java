5ed5cbf70d132dc46a45b58ac31ea89427f9b73e
Ronald Escobar <ronaldescobarj@gmail.com>
2018-06-05 04:29:25 -0400

    map

--- src/main/java/com/ucbcba/demo/controllers/HomeController.java
+++ src/main/java/com/ucbcba/demo/controllers/HomeController.java
@@ -1,6 +1,7 @@
-package com.ucbcba.demo.controllers;
+package com.ucbcba.demo.Controllers;
 
 import com.ucbcba.demo.entities.Category;
+import com.ucbcba.demo.entities.City;
 import com.ucbcba.demo.entities.Restaurant;
 import com.ucbcba.demo.services.CityService;
 import com.ucbcba.demo.services.RestaurantService;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,20 +34,85 @@ public class HomeController {
     }
 
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
+    public String welcome(Model model, @RequestParam(value = "searchFilter", required = false, defaultValue="") String searchFilter, @RequestParam(value = "cityDropdown", required = false, defaultValue="") String cityDropdown, @RequestParam(value = "showContent", required = false, defaultValue="") String showContent) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Boolean logged = (!getUserRole(auth).equals("notLogged"));
-
-
         model.addAttribute("role", getUserRole(auth));
         model.addAttribute("logged", logged);
         model.addAttribute("cities", cityService.listAllCities());
-        model.addAttribute("restaurants", restaurantService.listAllRestaurants());
-        model.addAttribute("searchFilter", "");
+        String search = "";
+        if (!searchFilter.equals("")) {
+            search = searchFilter;
+        }
+        model.addAttribute("search", search);
+
+        String citySelected = "";
+        if (!cityDropdown.equals("") || !cityDropdown.equals("All cities")) {
+            citySelected = cityDropdown;
+        }
+        model.addAttribute("citySelected", citySelected);
+
+        String showTable = "table";
+        if (showContent.equals("map")) {
+            showTable = showContent;
+        }
+        model.addAttribute("showTable", showTable);
+        List<Restaurant> allRestaurants = new ArrayList<>();
+        for (Restaurant restaurant : restaurantService.listAllRestaurants()) {
+            allRestaurants.add(restaurant);
+        }
+        List<Restaurant> filteredRestaurants = new ArrayList<>();
+        if (searchFilter.equals("") && (cityDropdown.equals("") || cityDropdown.equals("All cities"))) {
+            filteredRestaurants = allRestaurants;
+        } else {
+            if (!searchFilter.equals("") && (cityDropdown.equals("") || cityDropdown.equals("All cities"))) {
+                for (int i = 0; i < allRestaurants.size(); i++) {
+                    if (allRestaurants.get(i).getName().toLowerCase().contains(searchFilter.toLowerCase())
+                            || searchCategories(allRestaurants.get(i).getCategories(), searchFilter.toLowerCase())) {
+                        filteredRestaurants.add(allRestaurants.get(i));
+                    }
+                }
+            }
+            else {
+                if (searchFilter.equals("") && !cityDropdown.equals("") && !cityDropdown.equals("All cities")) {
+                    System.out.println("entra");
+                    for (int i = 0; i < allRestaurants.size(); i++) {
+                        if (allRestaurants.get(i).getCity().getName().equals(cityDropdown)) {
+                            filteredRestaurants.add(allRestaurants.get(i));
+                        }
+                    }
+                }
+                else {
+                    for (int i = 0; i < allRestaurants.size(); i++) {
+                        if ((allRestaurants.get(i).getName().toLowerCase().contains(searchFilter.toLowerCase())
+                                || searchCategories(allRestaurants.get(i).getCategories(), searchFilter.toLowerCase()))
+                                && allRestaurants.get(i).getCity().getName().equals(cityDropdown)) {
+                            filteredRestaurants.add(allRestaurants.get(i));
+                        }
+                    }
+                }
+            }
+        }
+        model.addAttribute("restaurants", filteredRestaurants);
+
+
+
+
+        List<Restaurant> temp = new ArrayList<>();
+        filteredRestaurants.forEach(temp::add);
+        List<Restaurant> restaurantsList = new ArrayList<>();
+        for (int i = 0; i < temp.size(); i++) {
+            Restaurant r = new Restaurant();
+            r.setName(temp.get(i).getName());
+            r.setLatitude(temp.get(i).getLatitude());
+            r.setLongitude(temp.get(i).getLongitude());
+            restaurantsList.add(r);
+        }
+        model.addAttribute("restaurantsList", restaurantsList);
         return "home";
     }
 
-    @RequestMapping(value = {"/search"}, method = RequestMethod.POST)
+    /*@RequestMapping(value = {"/search"}, method = RequestMethod.POST)
     public String search(Model model, String searchFilter) {
 
         List<Restaurant> restaurantList = new ArrayList<>();
@@ -65,7 +132,7 @@ public class HomeController {
         model.addAttribute("searchFilter", searchFilter);
         model.addAttribute("cities", cityService.listAllCities());
         return "home";
-    }
+    }*/
 
     private Boolean searchCategories(Set<Category> categories, String param) {
         for (Category category : categories) {
--- src/main/java/com/ucbcba/demo/entities/Restaurant.java
+++ src/main/java/com/ucbcba/demo/entities/Restaurant.java
@@ -29,6 +29,12 @@ public class Restaurant {
     @JoinColumn(name = "city_id")
     private City city;
 
+    @NotNull
+    private Float latitude;
+
+    @NotNull
+    private Float longitude;
+
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.REMOVE)
     private List<Comment> comments;
 
@@ -102,5 +108,21 @@ public class Restaurant {
 
     public void setPhoto(List<Photo> photos) {
         this.photo = photos;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
     }
 }
